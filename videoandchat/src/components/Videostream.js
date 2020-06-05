c883b0960fa4bd50d330218a1e4dc631988fc498
exactlyprateek <prateekrohilla4.pr@gmail.com>
2020-06-05 16:38:56 +0530

    chatapp integration

--- videoandchat/src/components/Videostream.js
+++ videoandchat/src/components/Videostream.js
@@ -1,6 +1,84 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import ChatF from './ChatF';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex'
+	},
+	appBar: {
+		transition: theme.transitions.create([ 'margin', 'width' ], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: drawerWidth,
+		transition: theme.transitions.create([ 'margin', 'width' ], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	menuButton: {
+		marginRight: theme.spacing(2)
+	},
+	hide: {
+		display: 'none'
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0
+	},
+	drawerPaper: {
+		width: drawerWidth
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: theme.spacing(0, 1),
+		// necessary for content to be below app bar
+		...theme.mixins.toolbar,
+		justifyContent: 'flex-end'
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		marginLeft: -drawerWidth
+	},
+	contentShift: {
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		}),
+		marginLeft: 0
+	}
+}));
+
 const mystyle = {
 	marginbottom: '30px',
 	maxwidth: '400px',
@@ -9,26 +87,102 @@ const mystyle = {
 const vidstyle = {
 	margin: '2%'
 };
-const Videostream = () => {
-	return (
-		<div>
-			{/* <input type="file" accept="video/*" />
-			<video controls autoplay /> */}
-			<Grid container direction="row" justify="flex-start" alignItems="center">
-				<Grid xs={12} sm={6} md={6}>
-					<video style={vidstyle} controls autoplay src="http://smoothcode.co/video/this.mp4" />
-					<Grid style={vidstyle}>
-						<h2 style={vidstyle} id="video-title">
-							props.name
-						</h2>
-						<p id="video-description">desc</p>
-					</Grid>
-				</Grid>
 
-				<Grid style={mystyle} xs={12} sm={4} md={4}>
-					<ChatF />
-				</Grid>
-			</Grid>
+const Videostream = () => {
+	const classes = useStyles();
+	const theme = useTheme();
+	const [ open, setOpen ] = React.useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<AppBar
+				position="fixed"
+				className={clsx(classes.appBar, {
+					[classes.appBarShift]: open
+				})}
+			>
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						onClick={handleDrawerOpen}
+						edge="start"
+						className={clsx(classes.menuButton, open && classes.hide)}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" noWrap>
+						Video Stream
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer
+				className={classes.drawer}
+				variant="persistent"
+				anchor="left"
+				open={open}
+				classes={{
+					paper: classes.drawerPaper
+				}}
+			>
+				<div className={classes.drawerHeader}>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+					</IconButton>
+				</div>
+				<Divider />
+				<List>
+					{[ 'Inbox', 'Starred', 'Send email', 'Drafts' ].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+				<Divider />
+				<List>
+					{[ 'All mail', 'Trash', 'Spam' ].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+			</Drawer>
+			<main
+				className={clsx(classes.content, {
+					[classes.contentShift]: open
+				})}
+			>
+				<div className={classes.drawerHeader} />
+
+				<div>
+					<Grid container direction="row" justify="flex-start" alignItems="center">
+						<Grid xs={12} sm={6} md={6}>
+							<video style={vidstyle} controls autoplay src="http://smoothcode.co/video/this.mp4" />
+							<Grid style={vidstyle}>
+								<h2 style={vidstyle} id="video-title">
+									props.name
+								</h2>
+								<p id="video-description">desc</p>
+							</Grid>
+						</Grid>
+
+						<Grid style={mystyle} xs={12} sm={4} md={4}>
+							<ChatF />
+						</Grid>
+					</Grid>
+				</div>
+			</main>
 		</div>
 	);
 };
